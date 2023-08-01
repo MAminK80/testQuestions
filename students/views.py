bfa803026a8fef56fa4cfcc7a1e463deb3cfc1b4
MAminK80 <107070284+MAminK80@users.noreply.github.com>
2023-08-01 17:13:53 +0330

    views.py

--- students/views.py
+++ students/views.py
@@ -1,64 +1,61 @@
 import random
-
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
-from django.views.generic import CreateView
-
-from . import final_score
 from Question.models import Question, Option, Category, FinalResult
+from .models import RandomQuestions
 from .forms import AnswerForm
 from .answers_module import Answer
 from django.core.paginator import Paginator
 from .page_number import Page
 from .final_score import Score
+from .question_module import ThisQuestion
+from .form_function import FormMaker
 
 
 class StudentView(View):
-    j = 0
-    choice_list = []
-    questions = Question.objects.all()
-    if bool(Question.objects.all()):
-        questions_list = [question for question in questions]
-        random.shuffle(questions_list)
-    final_list = []
 
     def get(self, request):
-        options_list = []
-        my_list = []
-        my_list.clear()
-        options_list.clear()
-        self.choice_list.clear()
-        answer = Answer(request)
         page = Page(request)
-        paginator = Paginator(self.questions_list, 1)
+
         num_page = page.current_page()
         if not bool(num_page):
             num_page = '1'
-        if num_page == 1:
-            answer.delete()
+        if num_page == '1':
+            questions = Question.objects.all()
+            list_questions = [q for q in questions]
+            random.shuffle(list_questions)
+
+            for i in list_questions:
+                RandomQuestions.objects.create(category=i.category.category_name,
+                                               question_text=i.question_text,
+                                               session_id=request.session)
+
+            r_questions = RandomQuestions.objects.all()
+
+        else:
+            r_questions = RandomQuestions.objects.all()
+
+        paginator = Paginator(r_questions, 1)
         paginated_questions = paginator.get_page(num_page)
-        for question in paginated_questions:
-            i = 0
-            self.choice_list.append(question.question_text)
-            options = Option.objects.filter(question=question)
-            for option in options:
-                i += 1
-                options_list.append((str(i), option.text))
-            my_list.append([question, options_list])
+        this_question = ThisQuestion(request)
         form = AnswerForm()
-        form.fields['field'].choices = my_list[0][1]
-        form.fields['field'].label = my_list[0][0].question_text
+        for question in paginated_questions:
+            this_question.next_question(question.question_text)
+            options = Option.objects.filter(question__question_text=question.question_text)
+            form_maker = FormMaker(options)
+            form.fields['field'].label = this_question.current_question()
+            form.fields['field'].choices = form_maker.options_maker()
+
         return render(request, 'students/students_view.html', {'form': form})
 
     def post(self, request):
         form = AnswerForm(data=request.POST)
         answer = Answer(request)
         page = Page(request)
+        this_question = ThisQuestion(request)
         if form.is_valid():
             choice = form.cleaned_data.get('field')
-            question = Question.objects.get(question_text=self.choice_list[0])
+            question = Question.objects.get(question_text=this_question.current_question())
             choice_ob = Option.objects.filter(question=question)
             selected_answer = choice_ob[int(choice) - 1]
             answer.add(question.question_text, selected_answer, selected_answer.score)
@@ -66,7 +63,7 @@ class StudentView(View):
             next_page = int(num_page) + 1
             if next_page > Question.counter.count():
                 page.next_page('1')
-                random.shuffle(self.questions_list)
+                RandomQuestions.objects.all().delete()
                 return redirect('score')
             page.next_page(str(next_page))
             return redirect('student_view')
