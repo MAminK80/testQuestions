352077f45d91c87e078c740c5ef94cb36c62b653
MAminK80 <107070284+MAminK80@users.noreply.github.com>
2023-08-15 16:23:05 +0330

    Delete accounts directory

--- accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-    phone = forms.CharField(
-        widget=forms.TextInput(attrs={'class': "form-control", 'placeholder': 'Your Phone Number'}))
-    password = forms.CharField(widget=forms.PasswordInput(
-        attrs={'class': "form-control", 'placeholder': 'Your Password'}))
--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'accounts'
-urlpatterns = [
-    path('login/', views.LoginUser.as_view(), name='login_user'),
-    path('logout/', views.LogoutUser.as_view(), name='logout_user')
-]
